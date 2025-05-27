074df528de276fe83010afbb1596b7b7e309aacb
DavidGarplid <s2308786@student.hb.se>
2025-05-27 11:42:15 +0200

    Menu button for loading scenes working

--- Assets/Scripts/LevelMenu/LoadScene.cs
+++ Assets/Scripts/LevelMenu/LoadScene.cs
@@ -8,7 +8,7 @@ public class LoadSceneOnClick : MonoBehaviour
 
     public void LoadScene()
     {
-        
+        Debug.Log($"Attempting to load scene: {sceneToLoad}");
         SceneManager.LoadScene(sceneToLoad);
     }
 }
