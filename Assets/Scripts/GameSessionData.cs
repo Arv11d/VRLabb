cd8b11495ee7fab75114338473ac69f054d45052
DavidGarplid <s2308786@student.hb.se>
2025-05-29 17:11:59 +0200

    repeated voiceline fix

--- Assets/Scripts/GameSessionData.cs
+++ Assets/Scripts/GameSessionData.cs
@@ -4,4 +4,5 @@ public static class GameSessionData
 {
     public static bool IsDungeonButtonUnlocked = false;
     public static bool IsDungeonButtonUnlocked2 = false;
+    public static bool HasPlayedDungeonVoiceLine = false;
 }
